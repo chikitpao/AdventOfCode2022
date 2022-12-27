8b487d9ea0f063e4ba448efef85c31fc14ca4ec3
Chi-Kit Pao <Chi-KitPao@gmx.de>
2022-12-28 00:05:02 +0100

    Added Solution for Day 20, Part 1

---
+++ Day20/day20.py
@@ -0,0 +1,69 @@
+""" Advent of Code 2022, Day 20
+    Author: Chi-Kit Pao
+"""
+
+
+import os
+
+def debug_output(message):
+    # print(message)
+    pass
+
+class Item:
+    def __init__(self, number: int):
+        self.number = number
+
+class Quiz:
+    def __init__(self, input_file_name: str):
+        self.original_list: list[Item] = []
+        self.working_list: list[Item] = []
+        self.__read_numbers(input_file_name)
+
+    def run(self):
+        length = len(self.working_list)
+        for i in range(len(self.original_list)):
+            item = self.original_list[i]
+            index = self.working_list.index(item)
+            assert index != -1
+            if item.number == 0:
+                continue
+            new_pos = (index + item.number) % (length - 1)
+            # # adjustment for left shift to begin -> move to end
+            # # Only to be conform to test example. Doesn't matter at all for 
+            # # circular list
+            # if new_pos == 0 and self.numbers[i] < 0:
+            #     new_pos = length - 1
+            self.working_list.insert(new_pos, self.working_list.pop(index))
+
+    def sum(self):
+        length = len(self.working_list)
+        index_of_0 = next(i for i, item in enumerate(self.working_list) if item.number == 0)
+        value_1000th = self.working_list[(index_of_0 + 1000) % length].number
+        value_2000th = self.working_list[(index_of_0 + 2000) % length].number
+        value_3000th = self.working_list[(index_of_0 + 3000) % length].number
+        debug_output(f'value_1000th {value_1000th} value_2000th {value_2000th} value_3000th {value_3000th}')
+        return value_1000th + value_2000th + value_3000th
+
+
+    def __read_numbers(self, input_file_name):
+        file_path = os.path.dirname(__file__)
+        with open(os.path.join(file_path, input_file_name), 'r') as f:
+            lines = list(map(lambda s: s.replace('\n', '').strip(), f.readlines()))
+
+        for line in lines:
+            if len(line) > 0:
+                item = Item(int(line))
+                self.original_list.append(item)
+                self.working_list.append(item)
+
+def main():
+    file_names = ['test.txt', 'input1.txt']
+    file_names_index = 1
+
+    quiz = Quiz(file_names[file_names_index])
+    print('# First question')
+    quiz.run()
+    print(f'Sum of three number: {quiz.sum()}')
+
+if __name__ == '__main__':
+    main()
